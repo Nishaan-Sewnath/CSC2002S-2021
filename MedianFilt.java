28c510a76c615dc1c56fef0a6c00abb444320b1f
Nishaan Sewnath <nishaan96@gmail.com>
2021-08-21 16:52:15 +0200

    almost at a breakthrough on the sequential part of the assignment

--- MedianFilt.java
+++ MedianFilt.java
@@ -6,9 +6,109 @@ import java.io.*;
 public class MedianFilt{
 
 
+	public static float[] getMedian(float[] arr, int filtS){
+
+		float temp =0.0f;
+		float num1 =0.0f;
+		float num2 =0.0f;
+		float num3 =0.0f;
+		int med =0;
+		float[] numArr = new float[filtS];
+		float[] result = new float[arr.length];
+
+
+		med = (filtS+1)/2;
+
+		for(int k = 0; k<arr.length; k++){
+
+
+			for(int z = 0; z<filtS; z++){
+
+				
+				if(!((k-med)<0)){
+
+					
+					numArr[z] = arr[k-med];
+
+
+
+				}
+
+
+
+			}
+			Arrays.sort(numArr);
+			
+			if(!((k-med)<0)){
+				result[k] = numArr[med];
+			}else{
+				result[k] = arr[k];
+
+			}
+		}
+		/*for(int i = 0; i < arr.length; i++){
+
+			if(i == 0){
+
+				if(arr[i]<arr[i+1]){
+
+					
+					result[i] = arr[i];
+
+
+				}else{
+
+					result[i] = arr[i+1];
+
+
+				}
+
+			}else if(i > arr.length-2){
+
+
+				if(arr[i]<arr[i+1]){
+
+					result[i] = arr[i];
+
+
+				}else{
+
+
+					result[i] = arr[i+1];
+
+
+				}
+
+			}else{
+
+				num1 = arr[i];
+				num2 = arr[i+1];
+				num3 = arr[i+2];
+
+				numArr[0] = num1;
+				numArr[1] = num2;
+				numArr[2] = num3;
+
+				Arrays.sort(numArr);
+
+
+				result[i] = numArr[1];
+
+			}
+
+
+
+		}*/
+
+		return result;
+
+
+	}
+
 	public static void main(String []args){
 
 		String[] ffl;
+		float[] result;
 		String temp = "";
 
 		System.out.println("Enter a file path and filter size: ");
@@ -29,7 +129,7 @@ public class MedianFilt{
 
 		}
 
-		System.out.println(nInput[0]);
+		System.out.println(nInput[1]);
 
 		try{
 
@@ -50,22 +150,37 @@ public class MedianFilt{
 
 			ffl = temp.split(" ");
 
-			int numE = (ffl.length);
+			int numE = Integer.parseInt(ffl[1]);
 
-			System.out.println(""+ numE);
+			//System.out.println(ffl[1]);
 			float[] Arr1 = new float[numE];
 
-			for(int i = 1; i < numE; i+=2){
+			for(int i = 1; i < numE+1; i++){
+
+				if(i!=1){
+
+					if(i%2==0){
+						Arr1[i-1] = Float.parseFloat(ffl[i+1]);
+
+						//System.out.println(""+Arr1[i-1]);
+						
+					}else{
 
 
-				Arr1[i-1] = Float.parseFloat(ffl[i]);
 
-				System.out.println(""+Arr1[i-1]);
-
+					}
+				}
 
 			}
 
-			int leng = Arr1.length - filtS;
+			result = getMedian(Arr1,filtS);
+
+			for(int j = 0; j<result.length; j++){
+
+
+				System.out.println(""+result[j]);
+
+			}
 
 			
 
